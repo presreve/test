4f67e2d2f487da0f0f19ca3cb9b3db1c9b39e447
presreve <143059169+presreve@users.noreply.github.com>
2023-08-30 19:22:14 +0800

    8.30

--- 源.cpp
+++ 源.cpp
@@ -1,218 +1,155 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<math.h>
 #include<string.h>
-#include<windows.h>
-#include<stdlib.h>
+#include"text.c"                 //引用自己创建的代码
 
-int x = 0;
-int y = 0;
-void Swap(int x, int y  )    //void表示没有返回值
-	{
-		int tmp = 0;
-        tmp = x;
-		x = y;
-		y = tmp;
-	}
-
+int Add(int x, int y);                           //声明函数放在   .h文件里
 int main()
 {
 	int a = 10;
 	int b = 20;
-	int tmp = 0;
-	printf("a=%d b=%d\n", a, b);
-	Swap(a, b);
-	//tmp = a;
-	//a = b;
-	//b= tmp;
-	printf("a=%d b=%d\n", a, b);
-
+	int sum = 0;
+	sum = Add(a, b);                   //函数调用
+	printf("%d\n", sum);
 	return 0;
+}
+int Add(int x, int y)                   //函数的定义放在   .c文件里
+{
+	int z = x + y;
+	return z;
 }
 
 
-
-
-
-// int get_max(int x, int y)
-//{
-//	 if (x > y)
-//		 return x;
-//	 else
-//		 return y;
-//}
 //int main()
 //{
-//	int a = 10;
-//	int b = 20;
-//	int max=get_max(a,b);
-//	printf("max=%d\n", max);
-//	 max = get_max(100, 200);
-//	printf("%d\n",max);
+//	printf("%d", printf("%d", printf("%d", 43)));
 //	return 0;
 //}
 
 
 
 //int main()
-//	{
-//	char arr[] = "hello world";       //memset     memory  内存   set  设置
-//	memset(arr, '*',7);
-//	printf("%s\n", arr);
+//{
+//	int len = 0;
+//	len = strlen("abc");
+//	printf("%d\n", len);
+//	printf("%d\n", strlen("abc"));     //链式访问
 //		return 0;
-//	}
-
-
-
-
-//int main()
-//{
-//	char arr1[] = "bit";           //strlen  --   string length--字符串长度有关
-//	char arr2[20] = "########";    //  strcpy  --  string copy  -- 字符串拷贝
-//	strcpy(arr2,arr1);
-//	printf("%s\n", arr2);
-//	return 0;
 //}
 
 
 
-//int Add(int x, int y)
+
+
+//void Add(int *p)
 //{
-//	int z = x + y;
-//	return z;
+//	(*p)++;
 //}
 //int main()
 //{
-//	int a = 10;
-//	int b = 20;
-//	int sum=Add(a, b);
-//	printf("%d\n", sum);
+//	int num = 0;
+//	Add(&num);
+//	printf("num=%d\n", num);//1
+//	Add(&num);
+//	printf("num=%d\n", num);//2
+//	Add(&num);
+//	printf("num=%d\n", num);//3
 //	return 0;
 //}
 
 
 
 
-//int main()
-//{
-//	char arr1[] = "welcome to bit";
-//	char arr2[] = "##############";
-//	int left = 0;
-//	int right = strlen(arr1) - 1;
-//	while (left <=right)
-//	{
-//		arr2[left] = arr1[left];
-//		arr2[right] = arr1[right];
-//		printf("%s\n", arr2);
-//		Sleep(500);
-//		system("cls");
-//		left++;
-//		right--;
-//
-//	}
-//	printf("%s\n",arr1);
-//	return 0;
-//}
 
-
-//int main()
+//                                   //本质上这里的arr是一个指针
+//int binary_search(int arr[], int k,int sz)       //
 //{
-//	char arr1[] = "welcome to bit";
-//	char arr2[] = "##############";
-//	//sz=sizeof (arr1[]）/sizeof （arr2[])
-//	int left = 0;
-//	int right = strlen(arr1) - 1;
+//	//int sz = sizeof(arr) / sizeof(arr[0]);      //这个计算指针是4字节
+//	int left = 0;                                      //算法的实现
+//	int right = sz - 1;
 //	while (left <= right)
 //	{
-//		arr2[left] = arr1[left];
-//		arr2[right] = arr1[right];
-//		printf("%s", arr2);
-//		Sleep(500);
-//		system("cls");
-//		left++;
-//		right--;
-//	}
-//	printf("%s", arr1);
-//	return 0;
-//}
-
-
-
-//int main()
-//{
-//	char arr[] = { 1,2,3,4,5,6,7,8,9 };
-//	int i = 1;
-//	int sz = 0;
-//	int k = 4;
-//	sz = sizeof(arr) / sizeof(arr[0]);
-//	for (i = 1; i < sz; i++)
-//	{
-//		if (k == arr[i])
+//		int mid = (right + left) / 2;
+//		if (arr[mid] < k)
 //		{
-//			printf("找到了，下标是：%d\n", i);
-//			break;
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
 //		}
 //	}
-//	if (i == sz)
-//		printf("找不到");
+//	return -1;
+//}
+//int main()
+//{
+//	                                 //二分查找
+//	                                 //在一个有序数组中查找某个数
+//	                                 //如果找到了返回这个的下表，找不到返回-1
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;                             //arr传过去的是arr首元素地址
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k, sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到指定的数字\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n",ret);
+//	}
 //	return 0;
 //}
 
 
+
+
+
+//int is_leap_year(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}
 //int main()
 //{
-//	char arr[] = { 1,2,3,4,5,6,7,8,9 };
-//	int k = 19;
-//	int sz = 0;
-//	int i = 0;
-//	//int arr = 0;
-//	sz = sizeof(arr) / sizeof(arr[0]);
-//	for (i = 0; i < sz; i++)
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
 //	{
-//		if (k == arr[i])
+//		if (1 == is_leap_year(year))                            //判断year是否为闰年
 //		{
-//			printf("找到了,下标是 %d\n", i);
-//			break;
-//		}
-//		//else
-//		//	printf("找不到");
+//			printf("%d ", year);
 //	}
-//	if(i==sz)
-//		printf("找不到");
+//	}
 //	return 0;
 //}
 
 
 
-//int main()
+//int is_prime(int n)             // 是素数返回1，不是返回0
 //{
-//	int i = 0;
-//	int q = 1;
-//	int n = 0;
-//	int sum = 0;
-//	scanf("%d", &n);
-//	for (i = 1; i <= n; i++)
+//	int j = 0;	                        //2-->n-1
+//	//for (j = 2; j < n; j++)
+//	for(j=2;j<=sqrt(n);j++)         //优化   试到开平方n
 //	{
-//		q = q * i;
-//		sum = sum + q;
+//		if (n % j == 0)
+//			return 0;
 //	}
-//	printf("sum=%d\n", sum);
-//	return 0;
+//	return 1;	                                //
 //}
-
-
-
-
 //int main()
 //{
-//	int i = 0;
-//	int n = 0;
-//	int q = 1;
-//	scanf("%d", &n);
-//	for (i = 1; i <= n; i++)
+//	int i = 0;                //打印100-200之间的素数
+//	for (i = 100; i <= 200; i++)
 //	{
-//		q = q * i;
+//		if (is_prime(i) == 1)                          //判断i是否为素数
+//			printf("%d\n", i);
 //	}
-//	printf("%d\n", q);
 //	return 0;
 //}
 
@@ -223,14 +160,47 @@ int main()
 
 //int main()
 //{
-//	int i = 0;
-//	int n = 0;
-//	int q = 1;
-//	scanf("%d", &n);
-//	for (i = 1; i <= n; i++)
-//	{
-//		q = q * i;
-//	}
-//	printf("%d", q);
+//	int a = 10;
+//	int*pa=&a;                    //pa是指针变量
+//	*pa = 20;                                 //解引用操作
+//	printf("%d\n", a);
 //	return 0;
 //}
+
+
+
+//当实参传给形参的时候
+//形参其实是实参的一份临时拷贝
+//对形参的修改是不会改变实参的
+//int x = 0;
+//int y = 0;
+//void Swap(int x, int y)    //void表示没有返回值
+//{
+//	int tmp = 0;
+//	tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+//
+//void Swap2(int*pa, int* pb)
+//{
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int tmp = 0;
+//	printf("a=%d b=%d\n", a, b);
+//	//Swap(a, b);
+//	Swap2(&a, &b);
+//	//tmp = a;
+//	//a = b;
+//	//b= tmp;
+//	printf("a=%d b=%d\n", a, b);
+//	return 0;
+//}
+ 
