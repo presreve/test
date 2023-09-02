21eaef38176b5f99fc0224f806e0d4b8381be548
presreve <143059169+presreve@users.noreply.github.com>
2023-09-02 14:23:31 +0800

    9.2数组

--- test.c
+++ test.c
@@ -4,10 +4,99 @@
 
 int main()
 {
-    char arr[] = "abcdef";   //  [a] [b] [c] [d] [e] [f] [\0] 
-    printf("%d\n", arr[3]);
+    int arr[3][4] = { {1,2,3,1},{5,6,7,8,} };
+    int i = 0;
+    for (i = 0; i < 3; i++)
+    {
+        int j = 0;
+        for (j = 0; j < 4; j++)
+        {
+            printf("&arr[%d][%d]=%p\n",i,j,&arr[i][j]);
+        }
+       
+    }
     return 0;
 }
+
+
+
+//int main()
+//{
+//    int arr[3][4] = { {1,2,3},{4,5} };        //三行四列的数组
+//    int i = 0;
+//    for (i = 0; i < 3; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < 4; j++) 
+//        {
+//            printf("%d ", arr[i][j]);
+//        }
+//       printf("\n");
+//    }
+
+    //char ch[5][6];         //五行六列数组
+
+    //int arr[] = { 1,2,3,4 };
+    //int arr[][4] = { {1,2,3,4},{5,6,7,8} };     //行可以省略，列不能省略
+
+  
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//    int sz = sizeof(arr) / sizeof(arr[0]);
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("arr(%d)=%p\n", i, &arr[i]);
+//}
+//    return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//    int sz = sizeof arr/ sizeof arr[0];
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//
+//    {
+//        printf("&arr[%d]=%p\n", i, &arr[i]);
+//    }
+//    return 0;
+//}
+
+
+
+
+//int main()
+//{
+   // char arr[] = "abcdef";   //  [a] [b] [c] [d] [e] [f] [\0] 
+   //// printf("%c\n", arr[3]);    //3是下标
+   // int i = 0;
+   // int len = strlen(arr);
+   // for (i = 0; i < len; i++)
+   // {
+   //     printf("%c ", arr[i]);
+   // }
+
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+//    int sz = sizeof(arr) / sizeof(arr[0]);
+//    int i = 0;
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
 
 
 //int main()
