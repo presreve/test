e281eb8117645b7f7f23e7cced3cf5ceb279fca4
presreve <143059169+presreve@users.noreply.github.com>
2023-09-03 13:28:25 +0800

    三子棋游戏

---
+++ game.h
@@ -0,0 +1,17 @@
+#pragma once
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+   //声明
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL],int row,int col);
+void ConmuterMove(char board[ROW][COL],int row, int col);
+//告诉我们四种状态
+//玩家赢         '*'
+//电脑赢         '#'
+//平局            'Q'
+//继续游戏      'C'
+char IsWin(char board[ROW][COL],int row,int col);
