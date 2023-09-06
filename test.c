c0410690e8277d71c8cec53e1c2e58f3efb55751
presreve <143059169+presreve@users.noreply.github.com>
2023-09-06 19:45:26 +0800

    9.6指针

--- test.c
+++ test.c
@@ -1,34 +1,247 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-//void test1(int arr[])
+
+
+
+
+//int main()
 //{
-//	printf("%d\n", sizeof(arr));//(2)
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* arr[] = { &a,&b,&c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", *arr[i]);
+//	}
+//	return 0;
 //}
-//void test2(char ch[])
+
+
+
+//int main()
 //{
-//	printf("%d\n", sizeof(ch));//(4)
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	/*int* pa = &a;
+//	int* pb = &b;
+//	int* pc = &c;*/
+//	   //整型数组-存放整型
+//	   //字符数组-存放数组
+//	  //指针数组-存放指针
+//	//int arr[10];
+//	int* arr2[3] = {&a,&b,&c};     //指针数组
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d\n", *(arr2[i]));
+//	}
+//	return 0;
 //}
+
+
+
+//
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int**ppa=&pa;      //ppa就是二级指针
+//	   //后面的*代表ppa是指针，前面的*是类型int*
+//	int*** pppa = ppa;    //pppa就是三级指针
+//	**ppa = 20;
+//	printf("%d\n", **ppa);
+//	printf("%d", a);
+//	return 0;
+//}
+
+
+
 //int main()
 //{
 //	int arr[10] = { 0 };
-//	char ch[10] = { 0 };
-//	printf("%d\n", sizeof(arr));//(1)
-//	printf("%d\n", sizeof(ch));//(3)
-//	test1(arr);
-//	test2(ch);
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		//printf("%d ", arr[i]);
+//		printf("%d ", *(p + i));
+//	}
+//
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("%p=====    %p\n",p+i, &arr[i]);
+//	//}
 //	return 0;
 //}
 
 
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);    //地址，首元素地址
+//	printf("%p\n", arr+1);
+//	printf("%p\n", &arr[0]);
+//	printf("%p\n", &arr[0]+1);
+//	printf("%p\n", &arr);
+//	printf("%p\n", &arr+1);
+//	//1. &arr---&数组名--数组名不是首元素地址--数组名表示整个数组
+//	             //&数组名  取出的是整个数组的地址
+//	//2. sizeof(arr)--sizeof(数组名)--数组名表示整个数组--
+//	                    //sizeof(数组名)计算的是整个数组的大小
+//	return 0;
+//}
+
+
+
+//int my_strlen(char*str)
+//{
+//	char* start = str;
+//	char* end = str;
+//	while (*end != '\0')
+//	{
+//		end++;
+//	}
+//	return end - start;
+//}
 //
 //int main()
 //{
-//	int a = 0;
-//	int b = 5;
-//	//int c = a && b;        //&&都为真才是真，有一个为假，就是假
-//	int c = a || b;              //有一个为真，就是真，两个都假才是假
-//	printf("%d\n", c);
+//	char arr[] = "12";
+//	int len=my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
+
+//int my_strlen(char*str)
+//{
+//	char* start = str;
+//	char* end = str;
+//	while (*end != '\0')
+//	{
+//		end++;
+//	}
+//	return end - start;
+//}
+//int main()
+//{
+//	//strlen   求字符串长度
+//	//递归--模拟实现了strlen--计数器的方式，递归的方式2
+//	char arr[] = "bit";
+//
+//	int len=my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char ch[5] = { 0 };
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", & arr[0] - &arr[9]);
+//	printf("%d\n", & arr[9] - &ch[0]);      //不能这样写，类型不一样，没有意义
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	int* p = &arr[9];
+//	//for (i = 0; i < sz; i++)
+//	//{
+//	//	printf("%d ", *p);
+//	//	p = p + 1;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", *p);
+//		p -= 2;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = arr;
+//	for (i = 0; i < sz; i++)
+//	{
+//
+//		printf("%d ", *p);
+//		p=p + 1;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	//int a = 10;
+//	//int* pa = &a;       //初始化
+//	//int* p = NULL;        //NULL是用来初始化指针的，给指针赋值
+//
+//	int a = 10;
+//	int* pa = &a;
+//	*pa = 20;
+//	//
+//	pa = NULL;
+//	*pa = 10;      //已经是空指针，不能使用
+//	//if (pa != NULL)
+//	//{
+//
+//	//}
+//	return 0;
+//}
+
+
+
+//int* test()
+//{
+//	int a = 10;
+//	return a;
+//	//int arr[10] = { 1 };
+//	//return &arr;
+//}
+//int main()
+//{
+//	int* p=test();
+//	printf("%d\n", *p);
+//	//*p = 20;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 12; i++)
+//	{
+//		*(p++);
+//	}
+//	  //
 //	return 0;
 //}
 
@@ -37,167 +250,76 @@
 
 //int main()
 //{
-//	int i = 0, a = 1, b = 2, c = 3, d = 4;
-//	//i = a++ && ++b && d++;    //a=0b=3c=3d=4
-//	i = a++||++b||d++;
-//	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+//	int a;     //局部变量不初始化，默认是随机值
+//	//int* p;    //局部的指针变量，就被初始化随机值
+//	int*p = 20;
+//	printf("%p\n", a);
 //	return 0;
 //}
-////程序输出的结果是什么？
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	char* p = arr;
+//	int*p=arr;                    //数组名，首元素 的地址
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = 1;
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* pa = &a;
+//	char* pc = &a;
+//	printf("%p\n", pa);
+//	printf("%p\n", pa+1);           //指针类型决定了指针走一步有多远（指针的步长）
+//	printf("%p\n", pc);               //int*p；p+1--->4
+//	printf("%p\n", pc+1);            //int*p；p+1--->1
+//	return 0;                             //double*p；p+1-->8
+//}
+
+
+
+//int main()
+//{
+//	/*printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(short*));
+//	printf("%d\n", sizeof(int*));
+//	printf("%d\n", sizeof(double*));*/
+//
+//	int a = 0x11223344;
+//	//int*pa=&a;
+//	//*pa = 0;
+//	char* pc = &a;             //指针类型决定了指针进行解引用操作时，能访问的空间的大小
+//	*pc = 0;                     //int*p时，解引用操作能访问4个字节
+//	//printf("%p\n", pa);      //char*p时，解引用操作能访问1个字节
+//	//printf("%p\n", pc);     //double*p，       能访问8个字节
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char s[] = "好好学习";
+//	printf("%s\n",s);
+//	return 0;
+//}
 
 
 //int main()
 //{
 //	int a = 10;
-//	int b = 20;
-//	int max = 0;
-//	//if (a > 5)
-//	//	b = 3;
-//	//else
-//	//	b = -3;
-//
-//	//b = (a > 5 ? 3 : -3);
-//	max=(a > b ? a : b);
-//	printf("%d\n", b);
+//	int*p=&a;               //指针变量
+//	printf("%d\n", p);
 //	return 0;
 //}
-
-
-
-//int main()
-//{
-//	int a = 1;
-//	int b = 2;
-//	int c = (a > b, a = b + 10, a, b = a + 1);
-//	printf("%d\n", c);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int a[10] = { 0 };
-//	a[4] = 10;        //[]就是操作符   a和4是操作数
-//	
-//	return 0;
-//}
-
-
-//get_max(int x, int y)      //这里的（）是语法规则
-//{
-//	return x > y ? x : y;
-//}
-//int main()
-//{
-//	int a = 10;
-//	int b = 20;
-//	            //  调用函数的时候的  ( )  就是函数调用的操作符
-//	int max = get_max(a, b);          //操作数是  get_max  a  b  三个   
-//	printf("%d\n", max);
-//	return 0;
-//}
-
-
-
-
-//学生
-//创建一个结构体类型-struct Stu
-//struct Stu
-//{
-//	char name[20];
-//	int age;
-//	char id[20];
-//};
-//int main()
-//{
-//	int a = 10;
-//	   //使用struct Stu这个类型创建了学生对象s1，并初始化
-//	struct Stu s1 = {"张三",20,"20120202"};
-//	struct Stu*ps=&s1;
-//
-//	 //  结构体指针  ->成员名
-//	printf("%s\n", ps->name);
-//	printf("%d\n", ps->age );
-//	printf("%s\n", ps->id );
-//
-//	
-//	//printf("%s\n", (*ps).name);
-//	//printf("%d\n", (*ps).age );
-//	//printf("%s\n", (*ps).id);
-//	 
-//	 
-//	 
-//	//printf("%d\n", s1.age);
-//	//printf("%s\n", s1.id);
-//	//printf("%s\n", s1.name);
-//	//   //结构体变量  .   成员名
-//	return 0;
-//}
-
-//int main()
-//{
-//	char a = 3;
-//	  // 000000000000000000000000  00000011
-//	  //00000011  -  a
-//
-//	char b = 127;
-//	             //0000000000000000000000000111 1111
-//	            //0111 1111   --b
-//	              //  a和b如何相加             从char变成整型
-//	             //00000000000000000000000000000011
-//	           // +
-//	           //0000000000000000000000000111 1111
-//	          //0000000000000000000000010000010
-//	          //char c=10000010
-//          	//11111111111111111111111110000010    整型提升     --补码
-//	        //11111111111111111111111110000001                     --反码
-//	         //10000000000000000000000001111110            --原码
-//
-//
-//	char c = a + b;
-//	printf("%d\n", c);
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	char a = 0xb6;
-//	short b = 0xb600;
-//	int c = 0xb6000000;
-//	if (a == 0xb6)
-//		printf("a");
-//	if (b == 0xb600)
-//		printf("b");
-//	if (c == 0xb6000000)
-//		printf("c");
-//	return 0;
-//}
-
-
-
-//int main()
-//{
-//	char c = 1;
-//	printf("%u\n", sizeof(c));
-//	printf("%u\n", sizeof(+c));
-//	printf("%u\n", sizeof(-c));
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	int a = 10;
-//	int b = 20;
-//	int c = b + a * 3;
-//	return 0;
-//}
-
-
-int main()
-{
-	int i = 1;
-	int a = (++i) + (++i) + (++i);
-	printf("%d\n", a);
-	return 0;
-}
