50a74c5a965eddcf2e974e2da6cc3bc9fd899fb7
presreve <143059169+presreve@users.noreply.github.com>
2023-09-11 23:12:53 +0800

    9.11C#

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////变量类型  变量名；
+            ////变量名=值
+            //int number;
+            //number = 100;
+            //int n = 100;
+            //double d = 3.14;
+            ////11111111111111111
+            ////00000000000000
+            //string Name = "张三";//字符串类型
+            //string s = "";//字符串可以空
+            ////字符串和字符区别    羊肉串和羊肉
+            //char gender = '男';//字符只能有一个，不能空
+            //decimal money = 5000m;
+
+            //int num = 10;
+            //Console.WriteLine(num);
+            //Console.ReadKey();
+            //float
+            //double d = 36.6;
+            //Console.WriteLine("d");
+
+            //int num1 = 10;
+            //int num2 = 20;
+
+            string name = "王五";
+            Console.WriteLine("你好，"+name);
+            Console.ReadKey();
+        }
+    }
+}
