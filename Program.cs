6a7c68a7f819389f388477fc5e51421a5e1cf082
presreve <143059169+presreve@users.noreply.github.com>
2023-09-14 19:14:51 +0800

    9.14C#

--- Program.cs
+++ Program.cs
@@ -4,28 +4,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _09上午小复习
+namespace _23.数组的5个练习
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region 数组练习1
+            ////练习1，从一个整数组中求出最大值，最小值，总和，平均值
+            //int[] nums = { 001, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int max = int.MinValue;//nums[0];
+            //int min = int.MaxValue;//nums[1];
+            //int sum = nums[2];
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] > max)
+            //    {
+            //        max = nums[i];
+            //    }
+            //    if (nums[i] < min)
+            //    {
+            //        min = nums[i];
+            //    }
+            //    sum += nums[i];
 
-            /*
-             * 变量类型： int double string char decimal bool
-             * 运算符：
-             * 赋值运算符：=
-             * 复合赋值运算符：+= -= *= /= %=
-             * 算数运算符：+-* / % ++ --
-             * 关系运算符：> < >= <= == !=
-             * 逻辑运算符：&&  ||  !
-             * 类型转换：
-             * 1.自动类型转换
-             * 2.强制类型转换
-             * 3.Convert
-             *
-             * 
-             */
+
+            //}
+            //Console.WriteLine("最大值是{0}，最小值是{1}，总和是{2}，平均值是{3}", max, min, sum, sum / nums.Length);
+            //Console.ReadKey(); 
+            #endregion
+
+            #region   数组练习3
+            ////练习3
+            //string str = null;
+            //string[] name = { "老杨", "老朱", "老虎", "老牛", "老赵" };
+            //for (int i = 0; i < name.Length-1; i++)
+            //{
+            //    str += name[i] + "|";
+            //}
+            //Console.WriteLine(str + name[name.Length-1]);
+            //Console.ReadKey(); 
+            #endregion
+
+            #region 数组练习4
+            ////练习4，使大于0的加1，小于0的减一
+            //int[] num = { 1, -2, 3, -4, 5, 6, 0 };
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    if (num[i] > 0)
+            //    {
+            //        num[i] += 1;
+            //    }
+            //    else if (num[i] < 0)
+            //    {
+            //        num[i] -= 1;
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //}
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+            //Console.ReadKey(); 
+            #endregion
+
+            #region 数组练习5
+
+            //string[] names = { "a", "b", "c", "d" };
+            //for (int i = 0; i < names.Length / 2; i++)
+            //{
+            //    string temp = names[i];
+            //    names[i] = names[names.Length - 1 - i];
+            //    names[names.Length - 1 - i] = temp;
+
+            //}
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+            //Console.ReadKey(); 
+            #endregion
+
+
+            string[] name = { "我", "是", "嫩", "蝶" };
+            for(int i = 0; i < name.Length/2; i++)
+            {
+                string temp = name[i];
+                name[i] = name[name.Length - 1-i];
+                name[name.Length - 1-i] = temp;
+
+            }
+            for(int i = 0;i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
