d0995f99b7ac2808d4ec11903215cb552c6a971a
presreve <143059169+presreve@users.noreply.github.com>
2023-09-15 10:43:31 +0800

    9.15C#语言

--- Program.cs
+++ Program.cs
@@ -4,104 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _23.数组的5个练习
+namespace _9._13方法函数
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region 数组练习1
-            ////练习1，从一个整数组中求出最大值，最小值，总和，平均值
-            //int[] nums = { 001, 2, 3, 4, 5, 6, 7, 8, 9 };
-            //int max = int.MinValue;//nums[0];
-            //int min = int.MaxValue;//nums[1];
-            //int sum = nums[2];
-            //for (int i = 0; i < nums.Length; i++)
-            //{
-            //    if (nums[i] > max)
-            //    {
-            //        max = nums[i];
-            //    }
-            //    if (nums[i] < min)
-            //    {
-            //        min = nums[i];
-            //    }
-            //    sum += nums[i];
-
-
-            //}
-            //Console.WriteLine("最大值是{0}，最小值是{1}，总和是{2}，平均值是{3}", max, min, sum, sum / nums.Length);
-            //Console.ReadKey(); 
-            #endregion
-
-            #region   数组练习3
-            ////练习3
-            //string str = null;
-            //string[] name = { "老杨", "老朱", "老虎", "老牛", "老赵" };
-            //for (int i = 0; i < name.Length-1; i++)
-            //{
-            //    str += name[i] + "|";
-            //}
-            //Console.WriteLine(str + name[name.Length-1]);
-            //Console.ReadKey(); 
-            #endregion
-
-            #region 数组练习4
-            ////练习4，使大于0的加1，小于0的减一
-            //int[] num = { 1, -2, 3, -4, 5, 6, 0 };
-            //for (int i = 0; i < num.Length; i++)
-            //{
-            //    if (num[i] > 0)
-            //    {
-            //        num[i] += 1;
-            //    }
-            //    else if (num[i] < 0)
-            //    {
-            //        num[i] -= 1;
-            //    }
-            //    else
-            //    {
-
-            //    }
-            //}
-            //for (int i = 0; i < num.Length; i++)
-            //{
-            //    Console.WriteLine(num[i]);
-            //}
-            //Console.ReadKey(); 
-            #endregion
-
-            #region 数组练习5
-
-            //string[] names = { "a", "b", "c", "d" };
-            //for (int i = 0; i < names.Length / 2; i++)
-            //{
-            //    string temp = names[i];
-            //    names[i] = names[names.Length - 1 - i];
-            //    names[names.Length - 1 - i] = temp;
-
-            //}
-            //for (int i = 0; i < names.Length; i++)
-            //{
-            //    Console.WriteLine(names[i]);
-            //}
-            //Console.ReadKey(); 
-            #endregion
-
-
-            string[] name = { "我", "是", "嫩", "蝶" };
-            for(int i = 0; i < name.Length/2; i++)
-            {
-                string temp = name[i];
-                name[i] = name[name.Length - 1-i];
-                name[name.Length - 1-i] = temp;
-
-            }
-            for(int i = 0;i < name.Length; i++)
-            {
-                Console.WriteLine(name[i]);
-            }
+            Program.PlayGame();
+            Program.WuDi();
+            Program.PlayGame();
+            Program.PlayGame();
+            Program.PlayGame();
+            Program.PlayGame();
+            Program.WuDi();
+            Program.PlayGame();
             Console.ReadKey();
+
+        }
+        /// <summary>
+        /// 正常玩游戏
+        /// </summary>
+        public static void PlayGame()
+        {
+            Console.WriteLine("超级玛丽走呀走，跳呀跳，顶呀顶");
+            Console.WriteLine("超级玛丽走呀走，跳呀跳，顶呀顶");
+            Console.WriteLine("超级玛丽走呀走，跳呀跳，顶呀顶");
+        }
+        /// <summary>
+        /// 无敌
+        /// </summary>
+        public static void WuDi()
+        {
+            Console.WriteLine("突然，顶到一个无敌");
+            Console.WriteLine("屏幕闪烁");
         }
     }
 }
